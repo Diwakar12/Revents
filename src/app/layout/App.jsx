767aca470104e1a62bfc80a6327f84c786871124
Devanshu <devanshu.raz@gmail.com>
2019-05-22 22:58:33 +0530

    event from update

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -15,30 +15,31 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
 
-      <Route
-        path="/(.+)"
-        render={() => (
-          <div>
-            <NavBar />
-            <Container className="main">
-              <Switch>
-                <Route path="/events" component={EventDashboard} />
-                <Route path="/test" component={TestComponent} />
-                <Route path="/event/:id" component={EventDetailedPage} />
-                <Route path="/people" component={PeopleDashboard} />
-                <Route path="/profile/:id" component={UserDetailedPage} />
-                <Route path="/settings" component={SettingsDashboard} />
-                <Route path="/createEvent" component={EventForm} />
-              </Switch>
-            </Container>
-          </div>
-        )}
-      />
-    </div>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/manage/:id" component={EventForm} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
+      </div>
     );
   }
 }
