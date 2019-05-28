aa2230a02c24f7e9fdc86e2f7947a5fb08bc29e3
Devanshu <devanshu.raz@gmail.com>
2019-05-28 23:16:23 +0530

    home page

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = ({history}) => {
   return (
-    <div>
+    
       <div className="ui inverted vertical masthead center aligned segment">
         <div className="ui text container">
           <h1 className="ui inverted stackable header">
@@ -20,24 +20,7 @@ const HomePage = ({history}) => {
           </div>
         </div>
       </div>
-      <div style={{ textAlign: 'center' }}>
-        Icons made by{' '}
-        <a href="http://www.freepik.com" title="Freepik">
-          Freepik
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>{' '}
-        is licensed by{' '}
-        <a
-          href="http://creativecommons.org/licenses/by/3.0/"
-          title="Creative Commons BY 3.0"
-        >
-          CC 3.0 BY
-        </a>
-      </div>
-    </div>
+     
   );
 };
 
